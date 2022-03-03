eab407b8aadaf79ece7ff1c6403f624179cf99c5
Unai Elejaga Jimeno <unai.elejaga@opendeusto.es>
2022-03-03 09:39:51 +0100

    Anyadido LoginScreen con Firebase

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwZvpIfvUtLr1NpEFG9C1g0xGY8CA7mkE",
+  authDomain: "my-app-20856.firebaseapp.com",
+  projectId: "my-app-20856",
+  storageBucket: "my-app-20856.appspot.com",
+  messagingSenderId: "524034589790",
+  appId: "1:524034589790:web:b2b7486277860618b2e0cf"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0 ) {
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app();
+}
+
+const auth = firebase.auth();
+export {auth}
--- navigation/StackNavigatior.js
+++ navigation/StackNavigatior.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import ProfileScreen from "../screens/ProfileScreen";
 import DrawerNavigator from "./DrawerNavigator";
 
 const Stack = createStackNavigator();
 
 const MainStackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home"   screenOptions={{headerShown: false}}>
+    <Stack.Navigator initialRouteName="Principal"   screenOptions={{headerShown: false}}>
       <Stack.Screen name="Principal" component={DrawerNavigator} />
       <Stack.Screen name="Settings" component={DrawerNavigator} />
       <Stack.Screen name="Perfil" component={DrawerNavigator} />
